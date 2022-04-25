341cb9e2d10eee1e61f3045b477f7c88d83cbb0a
Виктория Хлопина <linke16@yandex.ru>
2022-04-26 00:51:20 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int account = 300;
+        int refill = 300;
+        int bonus = refill / 100;
+
+        if (refill < 1000) {
+            //bonus = 0;
+        }
+        int total = account + refill + bonus;
+
+        System.out.println("Итого: " + total);
+        System.out.println("Бонусные рубли: " + bonus);
+
+        int account = 600;
+        int refill = 1500;
+        int bonus = refill / 100;
+
+        if (refill < 1000) {
+            bonus = 0;
+        }
+        int total = account + refill + bonus;
+
+        System.out.println("Итого: " + total);
+        System.out.println("Бонусные рубли: " + bonus);
+
+
+    }
+}
